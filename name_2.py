7b33f268bc09dc976b341455290d41eca548caec
Dmitry_Kartashev <kartashev.da@yandex.ru>
2025-12-10 15:01:56 +0300

    f_commit

---
+++ name_2.py
@@ -0,0 +1,8 @@
+# \t - табуляция
+# \n - новая строка
+
+first_name = "Dmitry"
+last_name = "Kartashev"
+full_name = f"{first_name} {last_name}"
+massage = f"\tHello, {full_name.title()}!"
+print(massage)
---
+++ pass.py
@@ -0,0 +1,5 @@
+favorite_landuage = ' python '
+print(favorite_landuage)
+print(favorite_landuage.lstrip())
+print(favorite_landuage.rstrip())
+print(favorite_landuage.strip())
